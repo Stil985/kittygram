1e1386dee14bb42fd01a58eb495b7c738b7acc8a
Stil985 <stil985@yandex.ru>
2024-01-18 23:45:44 +0300

    Изучил APIView и generics

--- cats/views.py
+++ cats/views.py
@@ -1,15 +1,59 @@
-from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APICatDetail(APIView):
+    def get(self, request, pk):
+        cat = get_object_or_404(Cat, pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        cat = get_object_or_404(Cat, pk=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        cat = get_object_or_404(Cat, pk=pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cat = get_object_or_404(Cat, pk=pk)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +61,34 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def hello(request):
+    if request.method == 'POST':
+        return Response({'message': 'Получены данные', 'data': request.data})
+
+    return Response({'message': 'Это был GET-запрос!'})
+
+
+@api_view(['GET', 'PATCH'])
+def cat_detail(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+    if request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_list, cat_detail, APICat, APICatDetail
 
 urlpatterns = [
    path('cats/', cat_list),
+   path('cats/<int:pk>/', cat_detail),
+   path('api/cats/', APICat.as_view()),
+   path('api/cats/<int:pk>/', APICatDetail.as_view()),
 ]
-
-
